f6bf5a672437135176f6975b305ed4b2182aaa4d
ryan255 <ryan255@163.com>
2016-04-14 18:29:34 +0800

    创建index.html，增加url模式，创建一个视图函数
    http://my.oschina.net/matrixchan/blog/184445?fromerr=4SM241Ah
    参考了上面的教程
    
    需要注意的点：增加url的时候如果用到include 要记得导入include
    from django.conf.urls import patterns, include, url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+
+from blog.models import BlogPost
+from django.template import loader,Context
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    posts = BlogPost.objects.all()
+    t = loader.get_template("index.html")
+    c = Context({'posts':posts})
+    return HttpResponse(t.render(c))
