5cf3c18f7caf1d402089fecebfd699351fe63184
ryan255 <ryan255@163.com>
2016-04-25 17:17:57 +0800

    增加css与js外部文件 优化界面

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import *
 from blog.views import index
-
+from mysite import settings
 urlpatterns = patterns('',
                       url(r'^$',index),
+                        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': settings.STATIC_URL}),
                       )
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), '../static/').replace('\\', '/'),
+)
