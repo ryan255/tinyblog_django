3564f20bb9c8adca225ea2ca3d8c772517c6e72e
ryan255 <ryan255@163.com>
2016-04-28 14:53:33 +0800

    增加多说评论模块 修改css

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'duoshuo'
 ]
+
+DUOSHUO_SECRET = 'xxxxx'
+DUOSHUO_SHORT_NAME = 'xxxxx'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
